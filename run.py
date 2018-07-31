1229480bb149750ed53e439cea20d8a7aa0ab0c4
sonjoonho <json0820@gmail.com>
2018-07-31 11:58:17 +0100

    Changed port for deployment

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask_wii import create_app, socketio
+import os
 
 # app = create_app(debug=True)
 app = create_app()
@@ -7,5 +8,5 @@ if __name__ == "__main__":
     import eventlet
     eventlet.monkey_patch()
 
-    # socketio.run(app, host="0.0.0.0", port="5000")
-    socketio.run(app)
+    port = int(os.environ.get("PORT", "5000"))
+    socketio.run(app, host="0.0.0.0", port=port)
