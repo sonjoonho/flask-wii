aaf90b80feebb528d1387ce3a0294c83275afb1e
sonjoonho <json0820@gmail.com>
2018-07-31 11:49:46 +0100

    Changed host for heroku

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     import eventlet
     eventlet.monkey_patch()
 
-    socketio.run(app, host="0.0.0.0", port="5000")
+    # socketio.run(app, host="0.0.0.0", port="5000")
+    socketio.run(app)
