8644cee34241ebb686b3670cffd85417938c8041
sonjoonho <json0820@gmail.com>
2018-08-01 18:58:38 +0100

    Implemented cursor movement and added comments

--- flask_wii/static/server/server.js
+++ flask_wii/static/server/server.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-  let socket = io.connect("https://" + document.domain + ":" + location.port + "/wii"); 
+  /** 
+   * Initialise socket.io
+   */
+
+  let socket = io.connect("http://" + document.domain + ":" + location.port + "/wii"); 
 
   const room = document.getElementById("room-number").innerHTML;
 
@@ -10,7 +14,6 @@ $(document).ready(function() {
 
   socket.on("disconnect", function() {
     console.log("Disconnected from room " + room);
-    socket.emit("join", {room: room});
   });
 
   socket.on("client_join", function(data) {
@@ -19,28 +22,29 @@ $(document).ready(function() {
     CursorObject(data.sid);
   });
 
+
+  /**
+   * Controls the cursor movement
+   */
+
   // Spawns a cursor
   let CursorObject = function(sid) {
     this.cursor = document.createElement("img");
     this.cursor.setAttribute("id", "cursor"+sid);
-    this.cursor.setAttribute("src", "/static/server/cursor.jpg");
+    this.cursor.setAttribute("src", "/static/server/cursor.png");
     this.cursor.setAttribute("width", "50");
     this.cursor.style.position = "absolute";
     document.body.appendChild(this.cursor);
   };
 
-  // Cursor movement
-
   screenWidth = window.screen.width * window.devicePixelRatio;
   screenHeight = window.screen.height * window.devicePixelRatio;
-
 
   socket.on("position", function(data) {
     position = data.position;
     sid = data.sid
     angles = data.angles
 
-    //console.log(angles);
     let alpha = angles.alpha;
     let beta = angles.beta;
     let gamma = angles.gamma;
@@ -55,7 +59,6 @@ $(document).ready(function() {
       y: position.y * screenHeight
     };
 
-    // cursor.style.left = (cursor.offsetLeft - cursorPosition.x) + "px"
     cursor.style.left = (cursorPosition.x) + "px";
     cursor.style.top = (cursorPosition.y) + "px";
     // Safari
@@ -64,46 +67,74 @@ $(document).ready(function() {
     cursor.style.msTransform = "rotate(" + gamma + "deg)";
     // Standard
     cursor.style.transform = "rotate(" + gamma + "deg)";
-    // console.log("x: "+position.x * screenWidth + " y: "+ position.y);
   });
 
-  // Button presses
+  /**
+   * Handling button presses
+   */
 
+  // Triggers a mouse event of eventType. This can be "click", "mousedown" or "mouseup".
+  function triggerMouseEvent(x, y, eventType) {
+      var clickEvent= document.createEvent('MouseEvents');
+      clickEvent.initMouseEvent(
+      eventType, true, true, window, 0,
+      0, 0, x, y, false, false,
+      false, false, 0, null
+      );
+      document.elementFromPoint(x, y).dispatchEvent(clickEvent);
+  }
+
+  // Triggers on A button down
   socket.on("a_down", function(data) {
     sid = data.sid
     let cursor = document.getElementById("cursor" + sid);
-    cursor.src = "static/server/cursor_grabbing.jpg";
+    cursor.src = "static/server/cursor_grabbing.png";
+    
+    // Gets the bounding box of the cursor
+    // Note that JavaScript and CSS handles .right and .bottom differently
+    let cursorPos = cursor.getBoundingClientRect();
+    console.log("Mouse down at " + cursorPos.top, cursorPos.left);
+    // Simulates the click event
+    
+    triggerMouseEvent(cursorPos.top, cursorPos.left, "mousedown");
 
-    if (overlap(close, cursor)) {
-      console.log("CLICK");   
-    }
+  
   });
 
+  // Triggers on A button up
   socket.on("a_up", function(data) {
     sid = data.sid
     let cursor = document.getElementById("cursor" + sid);
-    cursor.src = "static/server/cursor.jpg";
+    cursor.src = "static/server/cursor.png";
+    
+    let cursorPos = cursor.getBoundingClientRect();
+    console.log("Mouse up at " + cursorPos.top, cursorPos.left);
+    
+    triggerMouseEvent(cursorPos.top, cursorPos.left, "mouseup");
+
+  
   });
+
+  /**
+   * Controls the modal popup
+   */
 
   let  boot = document.getElementById("boot");
 
   let  close = document.getElementById("close");
   
-  //close.addEventListener("wii-click", function() {console.log("CLICK"); });
-
-  //let event = new Event("wii-click");
-
-  
-  // close.onclick = function() {
-  //     boot.style.display = "none";
-  // }
+  close.onmousedown = function() {
+    boot.style.display = "none";
+    console.log("CLICK");
+  };
 
   function overlap(rect1, rect2) { 
     
+    console.log(rect1.right, rect2.left);
     return (rect1.right < rect2.left || 
             rect1.left > rect2.right || 
             rect1.bottom < rect2.top || 
-            rect1.top > rect2.bottom)
+            rect1.top > rect2.bottom);
   }
 
 
