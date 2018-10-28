ad4dc3b3e128ffb14d270872b02966e8ad3b113b
sonjoonho <json0820@gmail.com>
2018-10-28 12:08:33 +0000

    Changed style of comment

--- flask_wii/server/routes.py
+++ flask_wii/server/routes.py
@@ -9,10 +9,10 @@ def index():
     if request.user_agent.platform == "android":
         return redirect(url_for("client.controller"))
     else:
-        """
-        FIXME: -- Need to keep track of rooms that exist and ensure that
-                  duplicate rooms are not generated.
-        """
+        
+        # FIXME: -- Need to keep track of rooms that exist and ensure that
+        #           duplicate rooms are not generated.
+       
         room_number = randint(1, 10000)
         return render_template("server/server.html", room_number=room_number)
 
