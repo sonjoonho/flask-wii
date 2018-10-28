7d84140093cd1d2ec40efa3ba4ba413c46c9f6b3
sonjoonho <json0820@gmail.com>
2018-10-28 12:09:00 +0000

    Update style

--- flask_wii/settings.py
+++ flask_wii/settings.py
@@ -2,11 +2,13 @@ class Config(object):
     DEBUG = False
     TESTING = False
 
+
 class DevelopmentConfig(Config):
     ENV = "development"
     DEBUG = True
     TESTING = True
     SECRET_KEY = "super secret"
+
 
 class ProductionConfig(Config):
     ENV = "production"
